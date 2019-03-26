22d8785a9231427d35ff53fefbf5f4e66b3dbb17
JamesTolputt <48034667+JamesTolputt@users.noreply.github.com>
2019-03-26 14:04:37 +1100

    Add files via upload

---
+++ this_is_a_thing_i_guess.ino
@@ -0,0 +1,27 @@
+void setup() {
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(line_estimation());
+}
+
+float line_estimation() {
+  const float x1 = 15, x2 = 5, x3 = -5, x4 = -15;
+  int w1 = analogRead(A0), w2 = analogRead(A0), w3 = analogRead(A0), w4 = analogRead(A0); 
+  int sum_of_weights = w1 + w2 + w3 + w4;
+  int sum_weight_distance = x1*w1 + x2*w2 + x3*w3 + x4*w4;
+
+  return sum_weight_distance/minimum(sum_of_weights, 1);
+}
+
+float minimum(float x, float y) {
+  if (x <= y)
+    return x;
+  return y;
+}
